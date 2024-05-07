fb899f6e0540048eb1f436fd2ddc14c86d0c9449
Kent Liu <kent.liu@rschooltoday.com>
2024-05-07 19:39:16 -0400

    freak mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Learn from './pages/Learn'; // import the component for /learn
@@ -11,20 +11,30 @@ import Piano from "./pages/Piano";
 import './App.css';
 
 function App() {
+
+  const [freakMode, setFreakMode] = useState(false);
+  const toggleFreakMode = () => {
+      setFreakMode(!freakMode);
+  };
+
   return (
     <Router>
-      <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/learn" element={<Learn />} />
-            <Route path="/play" element={<Play />} />
-            <Route path="/lesson/:id" element={<Lesson />} />
-            <Route path="/quiz/:id" element={<Quiz />} />
-              <Route path="/grad" element={<Grad />} />
-              <Route path="/start" element={<Start />} />
-              <Route path="/piano" element={<Piano />} />
-          </Routes>
-      </div>
+        <div className={`app ${freakMode ? 'freak-mode' : ''}`}>
+            {/*{freakMode && <link rel="stylesheet" href="../src/FreakMode.css"/>}*/}
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/learn" element={<Learn/>}/>
+                <Route path="/play" element={<Play/>}/>
+                <Route path="/lesson/:id" element={<Lesson/>}/>
+                <Route path="/quiz/:id" element={<Quiz/>}/>
+                <Route path="/grad" element={<Grad/>}/>
+                <Route path="/start" element={<Start/>}/>
+                <Route path="/piano" element={<Piano/>}/>
+            </Routes>
+            <button onClick={toggleFreakMode}>
+                {freakMode ? 'Exit Freak Mode' : 'Enter Freak Mode 👅'}
+            </button>
+        </div>
     </Router>
   );
 }

2d5b36a4e838f8367d1b031860a253a3aedce282
Kent Liu <kent.liu@rschooltoday.com>
2024-05-06 21:34:40 -0400

    lesson 3 content

--- frontend/src/pages/Lesson.jsx
+++ frontend/src/pages/Lesson.jsx
@@ -3,10 +3,12 @@ import { useParams } from 'react-router-dom';
 import Navbar from "../components/navbar";
 import Lesson1 from '../components/Lesson1';
 import Lesson2 from "../components/Lesson2";
+import Lesson3 from "../components/Lesson3";
 
 const lessonComponents = {
   "1": Lesson1,
   "2": Lesson2,
+  "3": Lesson3,
 };
 
 const Lesson = () => {

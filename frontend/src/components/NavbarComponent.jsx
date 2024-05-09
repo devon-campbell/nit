320d376248b6364ce4366d71b1364a0a00dadeb9
ErickCif <erickcifwork@gmail.com>
2024-05-09 00:30:52 -0400

    Daisyui implementation!

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -29,27 +29,27 @@ const NavbarComponent = ({ onToggle, freakMode, toggleFreakMode }) => {
   }, [onToggle]);
 
   return (
-    <nav ref={navbarRef} className="fixed top-0 w-full bg-gray-100 z-10" style={{ height: isOpen ? 'auto' : '50px' }}>
+    <nav ref={navbarRef} className="fixed top-0 w-full bg-accent z-10" style={{ height: isOpen ? 'auto' : '50px' }}>
       <div className="flex justify-between items-center p-1">
-        <button onClick={toggleMenu} className="text-3xl cursor-pointer pb-1 ml-2">
+        <button onClick={toggleMenu} className="text-3xl cursor-pointer pb-1 ml-2 text-neutral-content">
           ☰
         </button>
-        <Link to="/" className="text-2xl font-bold text-gray-800 font-sans flex-grow ml-2">
+        <Link to="/" className="text-2xl font-bold text-primary/80 font-sans flex-grow ml-2">
           nit
         </Link>
-        <button onClick={toggleFreakMode} className="mr-2">
+        <button onClick={toggleFreakMode} className="mr-2 text-neutral-content">
           {freakMode ? 'Exit Freak Mode' : 'Enter Freak Mode 👅'}
         </button>
       </div>
       <ul className={`${isOpen ? 'flex' : 'hidden'} flex-col items-start pl-4 w-full mt-2`}>
         <li>
-          <Link to="/learn" className="no-underline text-gray-500 hover:text-gray-950 hover:font-bold py-2 block">learn</Link>
+          <Link to="/learn" className="no-underline text-neutral-content hover:text-neutral-content/70 hover:font-bold py-2 block italic">learn</Link>
         </li>
         <li>
-          <Link to="/play" className="no-underline text-gray-500 hover:text-gray-950 hover:font-bold py-2 block">play</Link>
+          <Link to="/play" className="no-underline text-neutral-content hover:text-gray-950 hover:font-bold py-2 block italic">play</Link>
         </li>
         <li>
-          <Link to="/piano" className="no-underline text-gray-500 hover:text-gray-950 hover:font-bold py-2 block">piano (dev)</Link>
+          <Link to="/piano" className="no-underline text-neutral-content hover:text-gray-950 hover:font-bold py-2 block italic">piano (dev)</Link>
         </li>
       </ul>
     </nav>
